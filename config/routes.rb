e8c6b74c200130d8d700c758d26ac0c45b2e316c
Mai Anh Vu <me@maianhvu.com>
2015-08-02 18:33:53 +0800

    beautify routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   # Devise routes
   devise_for :users
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: 'register'
+    get 'sign-in', to: 'devise/sessions#new', as: 'sign_in'
+  end
 
   #Resource
   namespace :api, path: '/', constraints: { subdomain: 'api' } do
